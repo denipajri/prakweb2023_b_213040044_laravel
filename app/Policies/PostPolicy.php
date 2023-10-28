894bc8574ba33c99d21c6c042c97ee2b07733da6
denipajri <radenii2002@gmail.com>
2023-10-28 18:28:29 +0700

    menyelesaikan video 5

---
+++ app/Policies/PostPolicy.php
@@ -0,0 +1,45 @@
+<?php 
+
+namespace App\Policies;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class PostPolicy
+{
+    public function viewAny(User $user) : bool 
+    {
+        //
+    }
+
+    public function view(User $user, Post $post) : bool 
+    {
+        //
+    }
+    
+    public function create(User $user) : bool 
+    {
+        //
+    }
+    
+    public function update(User $user, Post $post) : bool 
+    {
+        //
+    }
+    
+    public function delete(User $user, Post $post) : bool 
+    {
+        //
+    }
+    
+    public function restore(User $user, Post $post) : bool 
+    {
+        //
+    }
+    
+    public function forceDelete(User $user, Post $post) : bool 
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,55 +30,5 @@ Route::get('/about', function () {
     ]);
 });
 
-
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Deni Pajri",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, nostrum? Asperiores dignissimos at quaerat officia, ullam consectetur iste ab minima labore cum culpa libero cupiditate aliquid rerum quisquam suscipit explicabo vero sed vel eveniet iusto. Pariatur perspiciatis nam corporis optio soluta porro harum nihil illo? Veniam voluptates, repudiandae quos velit possimus numquam dolores temporibus sequi commodi similique consectetur dicta placeat optio doloremque perspiciatis. Aut accusantium a repellat itaque, perspiciatis assumenda veniam? Aliquid consequatur quia, corporis voluptatibus vel dolor placeat beatae?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Agie Rivaldy",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam dolore illum officiis cumque iusto odio aut sequi ducimus quam, reiciendis eum facere quibusdam? Ad nihil doloribus porro tenetur, modi vitae! Expedita a enim asperiores! Totam similique molestiae impedit eaque natus dolorem odio perferendis at, modi cum repellat eum amet. Cupiditate mollitia nam iusto eaque possimus id, delectus modi nesciunt officia assumenda! Est maxime molestiae voluptatem, inventore ipsum debitis. Cum quidem rerum error natus magni recusandae deserunt mollitia architecto nesciunt nulla quasi fugiat quisquam eos a aperiam molestiae facilis, vero dolore! Cupiditate reprehenderit tenetur eveniet modi unde totam aliquid voluptatem magni?"
-        ]
-    ];
-
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
-
-// halaman single post
-Route::get('posts/{slug}', function ($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Deni Pajri",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, nostrum? Asperiores dignissimos at quaerat officia, ullam consectetur iste ab minima labore cum culpa libero cupiditate aliquid rerum quisquam suscipit explicabo vero sed vel eveniet iusto. Pariatur perspiciatis nam corporis optio soluta porro harum nihil illo? Veniam voluptates, repudiandae quos velit possimus numquam dolores temporibus sequi commodi similique consectetur dicta placeat optio doloremque perspiciatis. Aut accusantium a repellat itaque, perspiciatis assumenda veniam? Aliquid consequatur quia, corporis voluptatibus vel dolor placeat beatae?"
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Agie Rivaldy",
-            "body" => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam dolore illum officiis cumque iusto odio aut sequi ducimus quam, reiciendis eum facere quibusdam? Ad nihil doloribus porro tenetur, modi vitae! Expedita a enim asperiores! Totam similique molestiae impedit eaque natus dolorem odio perferendis at, modi cum repellat eum amet. Cupiditate mollitia nam iusto eaque possimus id, delectus modi nesciunt officia assumenda! Est maxime molestiae voluptatem, inventore ipsum debitis. Cum quidem rerum error natus magni recusandae deserunt mollitia architecto nesciunt nulla quasi fugiat quisquam eos a aperiam molestiae facilis, vero dolore! Cupiditate reprehenderit tenetur eveniet modi unde totam aliquid voluptatem magni?"
-        ]
-    ];
-
-    $new_post = [];
-    foreach($blog_posts as $post) {
-        if ($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('posts', [PostController::class, 'index']);
+Route::get('posts/{slug}', [PostController::class, 'show']);
