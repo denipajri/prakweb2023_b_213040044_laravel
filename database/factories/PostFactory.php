0d8a8c34fd9bffef129f0234c9ee6b72a297c944
denipajri <radenii2002@gmail.com>
2023-11-04 23:44:04 +0700

    menyelesaikan video 13 | searching & pagination

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -21,7 +21,7 @@ class PostFactory extends Factory
             'slug' => $this->faker->slug(),
             'excerpt' => $this->faker->paragraph(),
             'body' => collect($this->faker->paragraphs(mt_rand(8, 15)))
-                        ->map(fn($p) => "<p>$p</p>")
+                        ->map(fn ($p) => "<p>$p</p>")
                         ->implode(''),
             'user_id' => mt_rand(1, 3),
             'category_id' => mt_rand(1, 3)
