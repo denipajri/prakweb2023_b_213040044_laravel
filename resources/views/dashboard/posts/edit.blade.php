4fdd674dbc9a100e1a0afebcbb5d155b9aa73271
denipajri <radenii2002@gmail.com>
2023-11-08 22:14:25 +0700

    menyelesaikan video 22 | preview, update & delete image

--- resources/views/dashboard/posts/edit.blade.php
+++ resources/views/dashboard/posts/edit.blade.php
@@ -6,7 +6,7 @@
 </div>
 
 <div class="col-lg-8">
-    <form method="POST" action="/dashboard/posts/{{ $post->slug }}" class="mb-5">
+    <form method="POST" action="/dashboard/posts/{{ $post->slug }}" class="mb-5" enctype="multipart/form-data">
         @method('put')
         @csrf
         <div class="mb-3">
@@ -34,6 +34,23 @@
                 @endforeach
             </select>
         </div>
+
+        <div class="mb-3">
+            <label for="image" class="form-label">Post Image</label>
+            <input type="hidden" name="oldImage" value="{{ $post->image }}">
+            @if ($post->image)
+            <img src="{{ asset('storage/' . $post->image) }}" class="img-preview img-fluid mb-3 col-sm-5 d-block">
+            @else
+            <img class="img-preview img-fluid mb-3 col-sm-5">
+            @endif
+
+            <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image"
+                onchange="previewImage()">
+            @error('image')
+            <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </div>
+
         <div class="mb-3">
             <label for="body" class="form-label">Body</label>
             @error('body')
@@ -48,14 +65,28 @@
 
 <script>
     const title = document.querySelector('#title');
-        const slug = document.querySelector('#slug');
-        title.addEventListener('change', function() {
-            fetch('/dashboard/posts/checkSlug?title=' + title.value)
-                .then(response => response.json())
-                .then(data => slug.value = data.slug)
+    const slug = document.querySelector('#slug');
+    title.addEventListener('change', function() {
+        fetch('/dashboard/posts/checkSlug?title=' + title.value)
+            .then(response => response.json())
+            .then(data => slug.value = data.slug)
+    });
+    document.addEventListener('trix-file-accept', function(e) {
+        e.preventDefault();
+    });
+
+    function previewImage() {
+        const image = document.querySelector('#image');
+        const imgPreview = document.querySelector('.img-preview');
+        
+        imgPreview.style.display = 'block';
+        
+        const ofReader = new FileReader();
+        ofReader.addEventListener('load', function() {
+            imgPreview.src = `${this.result}`;
         });
-        document.addEventListener('trix-file-accept', function(e) {
-            e.preventDefault();
-        });
+        
+        ofReader.readAsDataURL(image.files[0]);
+    }
 </script>
 @endsection
